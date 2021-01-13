233d54f7f0aa60b68dc5aeaafb311eb007d30962
fromrawcom@gmail.com <fromrawcom@gmail.com>
2021-01-14 07:24:43 +0900

    background 설정
    css 변경하기

---
+++ js/bg.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 12;
+
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `././images/${imgNumber + 1}.jpg`;
+  body.appendChild(image);
+}
+
+function getRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+function init() {
+  const randomNumber = getRandom();
+  paintImage(randomNumber);
+}
+
+init();
